0b5c5e2e2a7c91317ca62577b102b90a0b3520ec
Shaaniaa <shaniaakhann64@gmail.com>
2020-06-24 00:20:13 +0500

    files

---
+++ app2.js
@@ -0,0 +1,110 @@
+
+//Chapter 21-25 Strings
+
+//Question 1
+/*
+var firstname = prompt("Enter your fir name: ")
+var lastname = prompt("Enter your last name: ")
+
+var fullname = firstname+" "+lastname
+
+alert("Welcome "+fullname)*/
+
+//Question 2
+
+/*
+var phone = prompt("My favorite phone is: ")
+var a = phone.length
+alert("Length of string: "+a)
+*/
+
+//Question 3
+
+/*
+var value = "Pakistani"
+var aa = value.indexOf("n")
+
+alert("String: "+value+"\nIndex of 'n': "+aa)
+*/
+
+//Question 4
+
+/*
+var val = "Hello World"
+var ans = val.lastIndexOf("l")
+alert("String: "+val+"\nIndex of 'n': "+ans)
+*/
+
+//Question 5
+/*
+var str = "Pakistani"
+var as = str.charAt(3)
+alert("String: "+str+"\nCharacter at index 3: "+as)
+*/
+
+//Question 6
+/*
+var firstname = prompt("Enter your first name: ")
+var lastname = prompt("Enter your last name: ")
+
+var fullname = firstname.concat(lastname)
+alert("Welcome "+fullname)
+*/
+
+//Question 7
+/*
+var city = "Hyderabad"
+var replacement = city.replace("Hyder","Islam")
+
+alert("City: "+city+"\nAfter replacement: "+replacement)
+*/
+
+//Question 8
+
+/*
+var message = "Ali and Sami are best friends. They play cricket and football together."
+var re= message.replace(/and/g,"&")
+
+alert("Message: "+message+"\nAfter replacement: "+re)
+*/
+
+//Question 9
+/*
+var value = "472"
+var answer = typeof(value)
+
+alert("Value: "+value+"\nType: "+answer)
+
+var anss= parseInt(value)
+var an = typeof(anss)
+alert("Value: "+value+"\nType: "+an)
+*/
+
+//Question 10
+/*
+var p = prompt("User input: ")
+var up = p.toUpperCase()
+
+alert("User input: "+p+"\nUpper case: "+up)
+*/
+
+//Question 11
+
+var k = prompt("User input: ")
+var up = k.toLocaleUpperCase()
+
+alert(up)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
